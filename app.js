d7b6cf5fe38ce8bf7910c74d2d18222135b0b19f
Peter <warrenp11@gmail.com>
2021-10-12 00:52:04 -0400

    generated the html file

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
+/*
 
-
+ARROW FUNCTIONS:
 
 const printProfileData = profileDataArr => {
     // !!! THIS CHANGES...
     // for (let i = 0; i < profileDataArr.length; i++) {
     //     console.log(profileDataArr[i]);
     // }
-    // !!! TO THIS...WHICH CHANGES...
+    // !!! TO THIS...
     // profileDataArr.forEach((profileItem) => {
     //     console.log(profileItem);
     // });
-    // !!! TO THIS
+    // !!! WHICH CHANGES...
+    // !!! TO THIS...
     profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
 printProfileData(profileDataArgs);
+
+*/
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error (err);
+
+    console.log('Portfoliio complete! Checkout out index.html to see the output!');
+});
